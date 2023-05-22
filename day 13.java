004e75c0d6794c19d95abfc081bd683d16d52b7b
Prince9555 <131245773+Prince9555@users.noreply.github.com>
2023-05-22 18:56:05 +0530

    Add files via upload

---
+++ day 13.java
@@ -0,0 +1,24 @@
+public class day 13 {
+    public static void public static void main(String[] args) {
+        
+    
+    
+     
+        public boolean isPowerOfThree(int n) {
+            if(n==0)
+                { return false;
+                }
+            else{
+                  while(n%3==0)
+                      n=n/3;
+                if(n==1)
+                    {return true;}
+    }
+            return false;
+            
+        }
+    }
+}
+        
+    
+
